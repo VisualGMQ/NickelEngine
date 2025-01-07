ebc9aced1b061cafcd1711029ded8343a03faf28
VisualGMQ <2142587070@qq.com>
2025-01-07 18:32:40 +0800

    forbid NICKE_API when not Win32

--- engine/engine/nickel/common/dllexport.hpp
+++ engine/engine/nickel/common/dllexport.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
 #ifdef NICKEL_DEF_DLLEXPORT
+#ifdef NICKEL_PLATFORM_WIN32
 #define NICKEL_API __declspec(dllexport)
-#else
+#endif
+#endif
+
+#ifndef NICKEL_API
 #define NICKEL_API
 #endif
