af860e62673188989589a3ce89b16f25bbdd6a67
VisualGMQ <2142587070@qq.com>
2025-01-22 17:54:22 +0800

    quiet find glslc

--- engine/cmake/compile_shader.cmake
+++ engine/cmake/compile_shader.cmake
@@ -1,6 +1,9 @@
 macro(compile_shader shader_name output_name)
-    message(STATUS "compiling shader ${CMAKE_CURRENT_SOURCE_DIR}: ${shader_name} --> ${output_name}")
-    execute_process(
-        COMMAND ${GLSLC_PROG}  ${CMAKE_CURRENT_SOURCE_DIR}/${shader_name} -o ${CMAKE_CURRENT_SOURCE_DIR}/${output_name}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+    if (GLSLC_PROG)
+        execute_process(
+            COMMAND ${GLSLC_PROG}  ${CMAKE_CURRENT_SOURCE_DIR}/${shader_name} -o ${CMAKE_CURRENT_SOURCE_DIR}/${output_name}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+    else()
+        message(STATUS "compiling shader ${CMAKE_CURRENT_SOURCE_DIR}: ${shader_name} --> ${output_name}")
+    endif()
 endmacro()
