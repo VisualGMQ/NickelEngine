eaaff133e12bc88c757593301552c6542db18561
VisualGMQ <2142587070@qq.com>
2025-01-25 00:08:20 +0800

    try fix ubuntu ci

--- engine/engine/nickel/graphics/bind_group.hpp
+++ engine/engine/nickel/graphics/bind_group.hpp
@@ -30,6 +30,8 @@ public:
 
     struct SamplerBinding final {
         Sampler sampler;
+
+        SamplerBinding() {}
     };
 
     struct CombinedSamplerBinding final {
@@ -47,7 +49,9 @@ public:
     };
 
     struct BindingPoint {
-        std::variant<BufferBinding, SamplerBinding, CombinedSamplerBinding,
+        BindingPoint() {}
+        
+        std::variant<SamplerBinding, BufferBinding, CombinedSamplerBinding,
                      ImageBinding>
             entry;
     };
