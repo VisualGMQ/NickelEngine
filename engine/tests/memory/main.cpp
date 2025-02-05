ec6c88d4a80a595133cf3e3683d87b8ca97a6f79
VisualGMQ <2142587070@qq.com>
2025-02-05 22:55:00 +0800

    prettier block memory allocator

--- engine/tests/memory/main.cpp
+++ engine/tests/memory/main.cpp
@@ -8,7 +8,10 @@ uint32_t gDestructCount = 0;
 struct Num {
     int num;
 
-    Num(int num): num{num} {}
+    Num(int num)
+        : num{num} {
+    }
+
     ~Num() { gDestructCount++; }
 };
 
@@ -55,12 +58,15 @@ TEST_CASE("block memory") {
         Num* value3 = allocator.Allocate(3);
         REQUIRE(allocator.UnuseCount(0) == 1);
         REQUIRE(gDestructCount == 0);
+
         allocator.Deallocate(value1);
         REQUIRE(allocator.UnuseCount(0) == 2);
         REQUIRE(gDestructCount == 1);
+
         allocator.Deallocate(value2);
         REQUIRE(allocator.UnuseCount(0) == 3);
         REQUIRE(gDestructCount == 2);
+
         allocator.Deallocate(value3);
         REQUIRE(allocator.UnuseCount(0) == 4);
         REQUIRE(gDestructCount == 3);
@@ -74,5 +80,63 @@ TEST_CASE("block memory") {
         REQUIRE(elem == nullptr);
         REQUIRE(allocator.InuseCount(0) == 0);
         REQUIRE(allocator.UnuseCount(0) == 4);
+    }
+
+    SECTION("GC") {
+        gDestructCount = 0;
+        BlockMemoryAllocator<Num> allocator(4);
+        Num* elem1 = allocator.Allocate(1);
+        Num* elem2 = allocator.Allocate(1);
+        Num* elem3 = allocator.Allocate(1);
+        allocator.MarkAsGarbage(elem1);
+        REQUIRE(allocator.InuseCount(0) == 2);
+        REQUIRE(allocator.UnuseCount(0) == 1);
+        REQUIRE(allocator.PendingDeleteCount(0) == 1);
+        REQUIRE(gDestructCount == 0);
+
+        allocator.GC();
+        REQUIRE(allocator.InuseCount(0) == 2);
+        REQUIRE(allocator.UnuseCount(0) == 2);
+        REQUIRE(allocator.PendingDeleteCount(0) == 0);
+        REQUIRE(gDestructCount == 1);
+    }
+
+    SECTION("GC with num") {
+        gDestructCount = 0;
+        BlockMemoryAllocator<Num> allocator(4);
+        Num* elems[8];
+        for (int i = 0; i < 8; i++) {
+            elems[i] = allocator.Allocate(1);
+        }
+        allocator.MarkAsGarbage(elems[0]);
+        allocator.MarkAsGarbage(elems[1]);
+        allocator.MarkAsGarbage(elems[2]);
+        allocator.MarkAsGarbage(elems[3]);
+
+        REQUIRE(allocator.PendingDeleteCount(0) == 4);
+        allocator.GC(2);
+
+        REQUIRE(allocator.BlockCount() == 2);
+        REQUIRE(allocator.InuseCount(0) == 0);
+        REQUIRE(allocator.UnuseCount(0) == 2);
+        REQUIRE(allocator.PendingDeleteCount(0) == 2);
+        REQUIRE(allocator.InuseCount(1) == 4);
+        REQUIRE(allocator.UnuseCount(1) == 0);
+        REQUIRE(allocator.PendingDeleteCount(1) == 0);
+        REQUIRE(gDestructCount == 2);
+    }
+
+    SECTION("reuse") {
+        gDestructCount = 0;
+        BlockMemoryAllocator<Num> allocator(4);
+        Num* elem1 = allocator.Allocate(1);
+        Num* elem2 = allocator.Allocate(2);
+        Num* elem3 = allocator.Allocate(3);
+        Num* elem4 = allocator.Allocate(4);
+
+        allocator.MarkAsGarbage(elem1);
+        Num* elem = allocator.RequireReuse();
+        REQUIRE(gDestructCount == 0);
+        REQUIRE(elem->num == 1);
     }
 }
