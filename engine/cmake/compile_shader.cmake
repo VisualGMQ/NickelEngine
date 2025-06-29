8f2d703f3c7bf739fd5f41be28a52e790293078a
VisualGMQ <2142587070@qq.com>
2025-06-29 20:08:41 +0800

    try fix compile error

--- engine/cmake/compile_shader.cmake
+++ engine/cmake/compile_shader.cmake
@@ -1,13 +1,15 @@
 macro(compile_shader shader_files)
-    foreach(file ${shader_files})
-        set(SHADER_COMPILED_FILE ${file}.spv)
-        add_custom_command(
-            OUTPUT ${SHADER_COMPILED_FILE}
-            COMMAND ${GLSLC_PROG} ${file} -o ${SHADER_COMPILED_FILE}
-            COMMENT "compiling shader ${file} -> ${SHADER_COMPILED_FILE}"
-            MAIN_DEPENDENCY ${file}
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-            VERBATIM
-        )
-    endforeach()
+    if (GLSLC_PROG)
+        foreach(file ${shader_files})
+            set(SHADER_COMPILED_FILE ${file}.spv)
+            add_custom_command(
+                OUTPUT ${SHADER_COMPILED_FILE}
+                COMMAND ${GLSLC_PROG} ${file} -o ${SHADER_COMPILED_FILE}
+                COMMENT "compiling shader ${file} -> ${SHADER_COMPILED_FILE}"
+                MAIN_DEPENDENCY ${file}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                VERBATIM
+            )
+        endforeach()
+    endif()
 endmacro()
